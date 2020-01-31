eea922d247e3fcd90a7b2554710a0bd41de1744d
Jonathan Fischoff <jfischoff@yahoo.com>
2020-01-30 22:44:03 -0800

    Simplify and use two connections

--- test/Hasql/NotificationSpec.hs
+++ test/Hasql/NotificationSpec.hs
@@ -18,6 +18,7 @@ import           Control.Concurrent
 import           Data.Foldable
 import           Control.Monad ((<=<))
 import           Data.ByteString (ByteString)
+import           Database.PostgreSQL.Simple.Options as Options
 
 aroundAll :: forall a. ((a -> IO ()) -> IO ()) -> SpecWith a -> Spec
 aroundAll withFunc specWith = do
@@ -43,21 +44,22 @@ aroundAll withFunc specWith = do
 
   beforeAll theStart $ afterAll theStop $ specWith
 
---withConn :: Temp.DB -> (Connection -> IO a) -> IO a
---withConn db f = do
--- let connStr = toConnectionString db
---  E.bracket (connectPostgreSQL connStr) close f
-
-
-withDBConn :: Temp.DB -> (Connection -> IO a) -> IO a
-withDBConn db f =
-  bracket (either (throwIO . userError . show) pure =<< acquire (toConnectionString db))
+withDBConn :: Options -> ((Connection, Connection) -> IO a) -> IO a
+withDBConn opts f =
+  bracket (either (throwIO . userError . show) pure =<< acquire (Options.toConnectionString opts))
           release
-          f
+     $ \conn -> bracket (either (throwIO . userError . show) pure =<< acquire (Options.toConnectionString opts))
+                release
+                $ \conn1 -> f (conn, conn1)
 
-withSetup :: (Connection -> IO ()) -> IO ()
+withSetup :: ((Connection, Connection) -> IO ()) -> IO ()
 withSetup f = either throwIO pure <=< withDbCache $ \dbCache ->
-  withConfig (verboseConfig <> cacheConfig dbCache) $ \db -> withDBConn db f
+  withConfig (verboseConfig <> cacheConfig dbCache) $ \db -> do
+    let localhostOpts = (toConnectionOptions db)
+          { host = pure "localhost"
+          }
+
+    withDBConn localhostOpts f
 
 getNotNonBlock :: Connection -> IO (Maybe Notification)
 getNotNonBlock = getNotificationNonBlocking' withConn
@@ -70,39 +72,47 @@ execute conn t = run (sql t) conn
 
 spec :: Spec
 spec = aroundAll withSetup $ do
+{-
   describe "getNotificationNonBlocking" $ do
     it "should not return a notification" $ \conn -> do
       _ <- execute conn "LISTEN channel"
       notification <- getNotNonBlock conn
       notification `shouldSatisfy` isNothing
+-}
 
-  describe "getNotification'" $ it "should return a notification" $ \conn -> do
+  describe "getNotification'" $ it "should return a notification" $ \(conn, conn1) -> do
     -- The lock ensures that getNotification can’t block the connection while it is waiting
-    lock <- newEmptyMVar
-    _ <- execute conn "LISTEN channel"
+    -- lock <- newEmptyMVar
+    _ <- execute conn1 "LISTEN channel"
     _ <-
      forkIO $
      do putStrLn "waiting for lock"
-        takeMVar lock
+        -- takeMVar lock
         putStrLn "got lock"
-        threadDelay 2000000
-        res <- withConn conn (\c -> exec c "NOTIFY channel")
+        threadDelay 1000000
+        --res <- withConn conn (\c -> exec c "NOTIFY channel")
+        res <- execute conn "NOTIFY channel"
         print res
         putStrLn "notified"
         pure ()
-    notification <-
-     getNotification' (\c f -> withConn c (test lock f))
-                      conn
-    notification `shouldSatisfy` isRight
+--    notification <-
+--     getNotification' (\c f -> withConn c (test lock f))
+--                      conn
+    Right _ <- getNotification conn1
+    -- notification `shouldSatisfy` isRight
+    pure ()
 
+{-
 test :: MVar () -> (t -> IO b) -> t -> IO b
 test lock f c = do putStrLn "filling lock"
                    result <- tryPutMVar lock ()
                    putStrLn $  "put result: " ++ show result
                    x <- f c
                    putStrLn "performed op on libpq conn"
-                   pure x
 
+                   pure x
+-}
+{-
 -- For some reason it seems to be important that the notify message is
 -- send using this function rather than simply combining sendQuery w
 -- ith getResult like hasql does. Otherwise random race conditions
@@ -117,7 +127,8 @@ exec h theSql =
           do mfd <- PQ.socket h
              case mfd of
                Nothing -> error "Database.PostgreSQL.Simple.Internal.exec"
-               Just _fd ->
+               Just fd ->
                  do -- Disabling threadWaitRead causes the bug
-                    -- threadWaitRead fd
+                    threadWaitRead fd
                     PQ.getResult h
+-}
